1ef262d3c003716150daace4200cd0cd7d746ae9
Moulaïli Saïdi <91431031+Moulayli@users.noreply.github.com>
2022-07-21 16:48:16 +0200

    Update maps.js

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -11,3 +11,55 @@ var mymap = L.map('map').setView([51.505, -0.09], 13);
 
 	L.marker([51.5, -0.09]).addTo(mymap)
 		.bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+
+const getdateortime = (date) => {
+    const date1 = new Date(date);
+    const date2 = new Date();
+    const diff = date2.getTime() - date1.getTime();
+    const diffSeconds = Math.floor(diff / 1000);
+    const diffMinutes = Math.floor(diff / 60000);
+    const diffHours = Math.floor(diff / 3600000);
+    const diffDays = Math.floor(diff / 86400000);
+    const diffWeeks = Math.floor(diff / 604800000);
+    const diffMonths = Math.floor(diff / 2629800000);
+    const diffYears = Math.floor(diff / 31557600000);
+    if (diffSeconds < 60) {
+      return "à l'instant";
+    } else if (diffMinutes < 60) {
+      if (diffMinutes === 1) {
+        return "il y a une minute";
+      } else {
+        return "il y a " + diffMinutes + " minutes";
+      }
+    } else if (diffHours < 24) {
+      if (diffHours === 1) {
+        return "il y a une heure";
+      } else {
+        return "il y a " + diffHours + " heures";
+      }
+    } else if (diffDays < 7) {
+      if (diffDays === 1) {
+        return "hier";
+      } else {
+        return "il y a " + diffDays + " jours";
+      }
+    } else if (diffWeeks < 4) {
+      if (diffWeeks === 1) {
+        return "il y a une semaine";
+      } else {
+        return "il y a " + diffWeeks + " semaines";
+      }
+    } else if (diffMonths < 12) {
+      if (diffMonths === 1) {
+        return "il y a un mois";
+      } else {
+        return "il y a " + diffMonths + " mois";
+      }
+    } else {
+      if (diffYears === 1) {
+        return "il y a un an";
+      } else {
+        return "il y a " + diffYears + " ans";
+      }
+    }
+  }
